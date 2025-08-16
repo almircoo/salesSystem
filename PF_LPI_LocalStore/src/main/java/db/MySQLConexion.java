6a5a76b193c35a0f82848b058a36df84b0d3bd0e
almircoo <almircoo@proton.me>
2025-08-16 10:12:30 -0500

    -

--- PF_LPI_LocalStore/src/main/java/db/MySQLConexion.java
+++ PF_LPI_LocalStore/src/main/java/db/MySQLConexion.java
@@ -12,7 +12,7 @@ public class MySQLConexion {
 			Class.forName("com.mysql.jdbc.Driver");
 			String url = "jdbc:mysql://localhost:3306/BD_CIBERTEC?useSSL=false&useTimezone=true&serverTimezone=UTC";
 			String usr = "root";
-			String psw = "Mrd1995**";
+			String psw = "hdhcgyooinmew2913736ggv3jy";
 			con = DriverManager.getConnection(url, usr, psw);
 		} catch (ClassNotFoundException e) {
 			System.out.println("Error >> Driver no Instalado!!" + e.getMessage());
